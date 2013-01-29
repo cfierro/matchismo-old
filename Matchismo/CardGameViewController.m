3a30817292400232ef823375f8b4fb97d7d8b015
cfierro <carlosfierro66@gmail.com>
2013-01-29 00:58:18 -0800

    Assignment 1

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -11,6 +11,7 @@
 #import "PlayingCard.h"
 #import "Deck.h"
 #import "CardMatchingGame.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
@@ -18,6 +19,14 @@
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (strong, nonatomic) CardMatchingGame *game;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+
+/*  The UI actually needs a label, but the text for this label is generated in CardMatchingGame.
+    CardMatchingGame keeps a history of the game and this is definitely part of the model. The label
+    is just a means to display the game history.
+ */
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *matchMode;
+@property (strong, nonatomic) IBOutlet UISlider *historySlider;
 @end
 
 @implementation CardGameViewController
@@ -38,15 +47,26 @@
 
 - (void)updateUI
 {
+    UIImage *cardBackImage = [UIImage imageNamed:@"cardBack.jpeg"];
     for (UIButton *cardButton in self.cardButtons) {
         Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
+        if (!card.isFaceUp) {
+            [cardButton setImage:cardBackImage forState:UIControlStateNormal];
+            cardButton.layer.cornerRadius = 6;
+            cardButton.clipsToBounds = YES;
+        }
+        else
+            [cardButton setImage:nil forState:UIControlStateNormal];
         [cardButton setTitle:card.contents forState:UIControlStateSelected];
         [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
         cardButton.selected = card.isFaceUp;
         cardButton.enabled = !card.isUnplayable;
-        cardButton.alpha = card.isUnplayable ? 0.3 : 1.0;
+        cardButton.alpha = card.isUnplayable ? 0.5 : 1.0;
     }
     self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
+    self.descriptionLabel.text = [self.game.gameHistory lastObject];
+    self.descriptionLabel.alpha = 1.0;
+    self.historySlider.value = 1.0;
 }
 
 - (void)setFlipCount:(int)flipCount
@@ -57,9 +77,36 @@
 
 - (IBAction)flipCard:(UIButton *)sender
 {
+    self.game.matchMode = self.matchMode.selectedSegmentIndex + 2;
     [self.game flipCardAtIndex:[self.cardButtons indexOfObject:sender]];
     self.flipCount++;
+    self.matchMode.enabled = NO;
     [self updateUI];
+}
+
+- (IBAction)reDeal:(UIButton *)sender
+{
+    self.flipCount = 0;
+    self.game = nil;
+    self.matchMode.selectedSegmentIndex = 0;
+    self.matchMode.enabled = YES;
+    for (UIButton *cardButton in self.cardButtons) {
+        cardButton.selected = NO;
+        cardButton.enabled = YES;
+        cardButton.alpha = 1.0;
+    }
+    [self updateUI];
+    self.descriptionLabel.text = @"Flip card to play!";
+}
+
+- (IBAction)viewHistory:(UISlider *)sender
+{
+    if ([self.game.gameHistory count]) {
+        int index = sender.value * ([self.game.gameHistory count] - 1);
+        self.descriptionLabel.text = [self.game.gameHistory objectAtIndex:index];
+        self.descriptionLabel.alpha = 0.5;
+        self.historySlider.value = sender.value;
+    }
 }
 
 @end
